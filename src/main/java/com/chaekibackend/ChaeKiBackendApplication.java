53d9ec3d1fef598035e7674a59f3553b75fa0f6e
xogus0226 <qkdrhks123@gmail.com>
2024-04-30 14:29:59 +0900

    [Init] 프로젝트 초기화

---
+++ src/main/java/com/chaekibackend/ChaeKiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chaekibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaeKiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaeKiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chaekibackend/ChaeKiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaekibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaeKiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
