1dec615474d5b346c576f7ddab1db6a448bc84a2
chjw956 <105613204+chjw956@users.noreply.github.com>
2024-05-07 20:39:02 +0900

    Update BookReview.java

--- src/main/java/com/chaekibackend/book/domain/entity/BookReview.java
+++ src/main/java/com/chaekibackend/book/domain/entity/BookReview.java
@@ -1,2 +1,34 @@
-package com.chaekibackend.book.domain.entity;public class BookReview {
+package com.chaekibackend.book.domain.entity;
+
+import com.chaekibackend.users.domain.entity.Users;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookReview {
+    @Id
+    @GeneratedValue
+    private Long no;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    @Column(nullable=false, columnDefinition="BOOLEAN DEFAULT true")
+    private Boolean visible;
+    @ManyToOne
+    @JoinColumn(name="USERS_ID")
+    private Users users;
+    @ManyToOne
+    private Book book;
+    @OneToMany
+    private List<BookReviewPhoto> photoList;
+
 }
