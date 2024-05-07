2d693343a0e5999da7c22f2bd6b6ea85200e536e
chjw956 <105613204+chjw956@users.noreply.github.com>
2024-05-07 20:39:30 +0900

    Update BookReviewReport.java

--- src/main/java/com/chaekibackend/book/domain/entity/BookReviewReport.java
+++ src/main/java/com/chaekibackend/book/domain/entity/BookReviewReport.java
@@ -1,2 +1,27 @@
-package com.chaekibackend.book.domain.entity;public class BookReviewReport {
+package com.chaekibackend.book.domain.entity;
+
+import com.chaekibackend.users.domain.entity.Users;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookReviewReport {
+    @Id
+    @GeneratedValue
+    private Long no;
+    private LocalDateTime createdAt;
+    @ManyToOne
+    private Users users;
+    @ManyToOne
+    private BookReview bookReview;
 }
