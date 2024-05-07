63faa9b525b62636eeb4d8da5ee4de6300589ff7
chjw956 <105613204+chjw956@users.noreply.github.com>
2024-05-07 20:40:55 +0900

    Update Coupon.java

--- src/main/java/com/chaekibackend/users/domain/entity/Coupon.java
+++ src/main/java/com/chaekibackend/users/domain/entity/Coupon.java
@@ -1,2 +1,26 @@
-package com.chaekibackend.users.domain.entity;public class Coupon {
+package com.chaekibackend.users.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coupon {
+    @Id
+    @GeneratedValue
+    private Long no;
+    private String serialNumber;
+    private float discountRate;
+    private String status;
+    private LocalDateTime expiredDate;
+    @ManyToOne
+    @JoinColumn(name="USERS_ID")
+    private Users users;
+
 }
