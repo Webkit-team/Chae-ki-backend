34bcd73148ade1bf927a70c589fac039741de9a5
chjw956 <105613204+chjw956@users.noreply.github.com>
2024-05-07 20:39:15 +0900

    Update BookReviewPhoto.java

--- src/main/java/com/chaekibackend/book/domain/entity/BookReviewPhoto.java
+++ src/main/java/com/chaekibackend/book/domain/entity/BookReviewPhoto.java
@@ -1,2 +1,21 @@
-package com.chaekibackend.book.domain.entity;public class BookReviewPhoto {
+package com.chaekibackend.book.domain.entity;
+
+import jakarta.annotation.ManagedBean;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookReviewPhoto {
+    @Id
+    @GeneratedValue
+    private Long no;
+    private String imageString;
+    @ManyToOne
+    @JoinColumn(name="BOOKREVIEW_ID")
+    private BookReview bookReview;  // 이것도 OneToMany로 리스트 갖고 있어야 할 듯
 }
