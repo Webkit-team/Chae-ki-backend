1c6a45e7b413c257dd1438b3539425b8b6c36d79
chjw956 <105613204+chjw956@users.noreply.github.com>
2024-05-07 20:35:37 +0900

    Update ChaekiToday.java

--- src/main/java/com/chaekibackend/chellenge/domain/entity/ChaekiToday.java
+++ src/main/java/com/chaekibackend/chellenge/domain/entity/ChaekiToday.java
@@ -1,2 +1,28 @@
-package com.chaekibackend.chellenge.domain.entity;public class ChaekiToday {
+package com.chaekibackend.chellenge.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChaekiToday {
+    @Id
+    @GeneratedValue
+    private Long no;
+    private String content;
+    private LocalDateTime createdAt;
+    private Integer likeCount;
+    private Boolean visible;
+    @ManyToOne
+    @JoinColumn(name="CHAEKIWEEK_ID")
+    private ChaekiWeek chaekiWeek;
+    @ManyToOne
+    @JoinColumn(name="CHALLENGEMEMBER_ID")
+    private ChallengeMember challengeMember;
 }
