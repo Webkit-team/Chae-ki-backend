a020dd9fc4baa2641c9c490919c575e315a5ec51
chjw956 <105613204+chjw956@users.noreply.github.com>
2024-05-07 20:39:48 +0900

    Update BookReviewScrap.java

--- src/main/java/com/chaekibackend/book/domain/entity/BookReviewScrap.java
+++ src/main/java/com/chaekibackend/book/domain/entity/BookReviewScrap.java
@@ -1,2 +1,24 @@
-package com.chaekibackend.book.domain.entity;public class BookReviewScrap {
+package com.chaekibackend.book.domain.entity;
+
+import com.chaekibackend.users.domain.entity.Users;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookReviewScrap {
+    @Id
+    @GeneratedValue
+    private Long no;
+    private LocalDateTime createdAt;
+    @ManyToOne
+    @JoinColumn(name="USERS_ID")
+    private Users users;
+
 }
