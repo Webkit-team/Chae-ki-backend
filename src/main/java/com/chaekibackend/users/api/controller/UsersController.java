57b63a94e437f8cb8ee66c6ff512787ce1568aad
xogus0226 <qkdrhks123@gmail.com>
2024-05-09 15:29:53 +0900

    [Update] 스웨거 문서화 설정 변경

--- src/main/java/com/chaekibackend/users/api/controller/UsersController.java
+++ src/main/java/com/chaekibackend/users/api/controller/UsersController.java
@@ -4,9 +4,11 @@ import com.chaekibackend.users.api.request.UsersRequest;
 import com.chaekibackend.users.api.response.UsersResponse;
 import com.chaekibackend.users.application.UsersAppService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,19 +19,19 @@ import org.springframework.web.multipart.MultipartFile;
 public class UsersController {
     private final UsersAppService usersAppService;
 
-    @DeleteMapping("/users/{uno}")
+    @DeleteMapping(value = "/users/{uno}", produces = {MediaType.APPLICATION_JSON_VALUE})
     @Operation(summary = "회원탈퇴", description = "회원탈퇴를 진행합니다.")
     public UsersResponse.Delete deleteUser(@PathVariable Long uno) {
         return usersAppService.deleteUser(uno);
     }
 
-    @PostMapping("/signup")
+    @PostMapping(value = "/signup", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
     @Operation(summary = "회원가입", description = "새로운 사용자를 등록합니다.")
     public UsersResponse.Create signup(
-            @RequestParam String username,
-            @RequestParam String password,
-            @RequestParam String nickname,
-            @RequestParam(required = false) MultipartFile image
+            @RequestPart(value = "username") String username,
+            @RequestPart(value = "password") String password,
+            @RequestPart(value = "nickname") String nickname,
+            @RequestPart(value = "image", required = false) @Parameter(description = "프로필 이미지 파일") MultipartFile image
     ) {
         UsersRequest.Create user = UsersRequest.Create.builder()
                 .username(username)
@@ -40,13 +42,14 @@ public class UsersController {
         return usersAppService.signup(user, image);
     }
 
-    @PutMapping("/users/{uno}")
+    @PutMapping(value = "/users/{uno}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
     @Operation(summary = "회원수정", description = "사용자의 정보를 수정합니다.")
     public UsersResponse.Update updateUser(
             @PathVariable Long uno,
-            @RequestParam String password,
-            @RequestParam String nickname,
-            @RequestParam(required = false) MultipartFile image
+            @RequestPart(value = "password", required = false) String password,
+            @RequestPart(value = "nickname", required = false) String nickname,
+            @Parameter(description = "프로필 이미지 파일")
+            @RequestPart(value = "image", required = false) MultipartFile image
     ) {
         UsersRequest.Update users = UsersRequest.Update.builder()
                 .password(password)
