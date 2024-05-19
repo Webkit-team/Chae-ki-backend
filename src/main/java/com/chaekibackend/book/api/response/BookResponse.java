6e1471e196962fb69919163f4a9a04f74c0ca98f
Gogumi <chjw956@naver.com>
2024-05-19 20:04:45 +0900

    [Update] 마이페이지 챌린지 목록 조회 기능 오류 수정

--- src/main/java/com/chaekibackend/book/api/response/BookResponse.java
+++ src/main/java/com/chaekibackend/book/api/response/BookResponse.java
@@ -41,13 +41,17 @@ public class BookResponse {
 
         public static BookResponse.Detail from(Book book) {
             // (데이터 가공) 응답Dto(BookResponse.Detail)에서 지은이와 옮긴이를 분리해서 담아줌
-            String[] writers = book.getWriter().split("(지은이)");
+            String[] writers = book.getWriter().split("\\(지은이\\)");
             String writer = writers[0];
+
             String translator = null;
+
             if(writers.length > 1) {
                 String splicedStr = writers[1].substring((writers[1].indexOf(("("))));
-                translator = splicedStr.substring(2, splicedStr.length() - 1);
+                translator = splicedStr.substring(2, splicedStr.length()-1);
             }
+
+            System.out.println("translator = " + translator);
 
             return Detail.builder()
                     .no(book.getNo())
